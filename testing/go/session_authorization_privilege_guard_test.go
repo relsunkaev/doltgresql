34890c9bdf437ebd071be74c661ba2e4e20182b1
Ramazan Elsunkaev <relsunkaev@outlook.com>
2026-05-12 16:48:06 -0700

    test: guard session authorization privilege boundary

---
+++ testing/go/session_authorization_privilege_guard_test.go
@@ -0,0 +1,64 @@
+// Copyright 2026 Dolthub, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package _go
+
+import (
+	"testing"
+
+	"github.com/dolthub/go-mysql-server/sql"
+)
+
+func TestSetSessionAuthorizationRequiresSuperuserForOtherInitialUsers(t *testing.T) {
+	RunScripts(t, []ScriptTest{
+		{
+			Name: "SET SESSION AUTHORIZATION rejects other roles for normal users",
+			SetUpScript: []string{
+				`CREATE USER session_auth_actor PASSWORD 'actor';`,
+				`CREATE USER session_auth_target PASSWORD 'target';`,
+			},
+			Assertions: []ScriptTestAssertion{
+				{
+					Query:    `SELECT current_user, session_user;`,
+					Expected: []sql.Row{{"session_auth_actor", "session_auth_actor"}},
+					Username: `session_auth_actor`,
+					Password: `actor`,
+				},
+				{
+					Query:    `SET SESSION AUTHORIZATION session_auth_actor;`,
+					Username: `session_auth_actor`,
+					Password: `actor`,
+				},
+				{
+					Query:    `SELECT current_user, session_user;`,
+					Expected: []sql.Row{{"session_auth_actor", "session_auth_actor"}},
+					Username: `session_auth_actor`,
+					Password: `actor`,
+				},
+				{
+					Query:       `SET SESSION AUTHORIZATION session_auth_target;`,
+					ExpectedErr: `permission denied`,
+					Username:    `session_auth_actor`,
+					Password:    `actor`,
+				},
+				{
+					Query:    `SELECT current_user, session_user;`,
+					Expected: []sql.Row{{"session_auth_actor", "session_auth_actor"}},
+					Username: `session_auth_actor`,
+					Password: `actor`,
+				},
+			},
+		},
+	})
+}
