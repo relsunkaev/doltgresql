d42058c63e9cc0a9c9de3ecfe6331210a2ae6719
Ramazan Elsunkaev <relsunkaev@outlook.com>
2026-05-12 17:15:15 -0700

    fix: preserve numeric sqrt precision

--- server/functions/sqrt.go
+++ server/functions/sqrt.go
@@ -16,6 +16,7 @@ package functions
 
 import (
 	"math"
+	"math/big"
 
 	"github.com/cockroachdb/errors"
 	"github.com/dolthub/go-mysql-server/sql"
@@ -24,6 +25,8 @@ import (
 	"github.com/dolthub/doltgresql/server/functions/framework"
 	pgtypes "github.com/dolthub/doltgresql/server/types"
 )
+
+const numericSqrtOutputPrecision = int32(15)
 
 // initSqrt registers the functions to the catasqrt.
 func initSqrt() {
@@ -55,10 +58,36 @@ var sqrt_numeric = framework.Function1{
 	Parameters: [1]*pgtypes.DoltgresType{pgtypes.Numeric},
 	Strict:     true,
 	Callable: func(ctx *sql.Context, _ [2]*pgtypes.DoltgresType, val1 any) (any, error) {
-		if val1.(decimal.Decimal).Cmp(decimal.Zero) == -1 {
+		val := val1.(decimal.Decimal)
+		if val.Cmp(decimal.Zero) == -1 {
 			return nil, errors.Errorf("cannot take square root of a negative number")
 		}
-		// TODO: decimal's Pow function does not work correctly using an exponent of 0.5, need to fix
-		return decimal.NewFromFloat(math.Sqrt(val1.(decimal.Decimal).InexactFloat64())), nil
+		result, err := val.PowWithPrecision(decimal.New(5, -1), numericSqrtOutputPrecision)
+		if err != nil {
+			return nil, err
+		}
+		return trimDecimalTrailingZeros(result.Round(numericSqrtOutputPrecision)), nil
 	},
+}
+
+func trimDecimalTrailingZeros(d decimal.Decimal) decimal.Decimal {
+	exp := d.Exponent()
+	if exp >= 0 {
+		return d
+	}
+
+	coeff := d.Coefficient()
+	ten := big.NewInt(10)
+	zero := big.NewInt(0)
+	remainder := new(big.Int)
+	for exp < 0 {
+		quotient := new(big.Int)
+		quotient.QuoRem(coeff, ten, remainder)
+		if remainder.Cmp(zero) != 0 {
+			break
+		}
+		coeff = quotient
+		exp++
+	}
+	return decimal.NewFromBigInt(coeff, exp)
 }
