21e0b7afcad7f851fbe1b7832bb3503d32b645a9
Ramazan Elsunkaev <relsunkaev@outlook.com>
2026-05-13 10:58:17 -0700

    fix: resolve uuid ossp functions in extension schema

--- server/functions/framework/provider.go
+++ server/functions/framework/provider.go
@@ -284,6 +284,18 @@ var compiledExtensionFunctionNames = map[string]map[string]struct{}{
 		"svals":                 {},
 		"tconvert":              {},
 	},
+	"uuid-ossp": {
+		"uuid_generate_v1":   {},
+		"uuid_generate_v1mc": {},
+		"uuid_generate_v3":   {},
+		"uuid_generate_v4":   {},
+		"uuid_generate_v5":   {},
+		"uuid_nil":           {},
+		"uuid_ns_dns":        {},
+		"uuid_ns_oid":        {},
+		"uuid_ns_url":        {},
+		"uuid_ns_x500":       {},
+	},
 }
 
 func compiledExtensionFunction(ctx *sql.Context, schemaName string, functionName string) (sql.Function, bool) {
