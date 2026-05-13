9b4919e1c815e40cee496842f340c8ae69871b60
Ramazan Elsunkaev <relsunkaev@outlook.com>
2026-05-12 19:47:41 -0700

    fix: make create table as atomic

--- server/node/create_table_as.go
+++ server/node/create_table_as.go
@@ -16,6 +16,8 @@ package node
 
 import (
 	"fmt"
+	"io"
+	"sync/atomic"
 
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/plan"
@@ -31,6 +33,8 @@ type CreateTableAs struct {
 }
 
 var _ sql.ExecBuilderNode = (*CreateTableAs)(nil)
+
+var createTableAsSavepointCounter uint64
 
 func NewCreateTableAs(tableCopier *plan.TableCopier) *CreateTableAs {
 	var createTable *plan.CreateTable
@@ -76,22 +80,47 @@ func (c *CreateTableAs) BuildRowIter(ctx *sql.Context, b sql.NodeExecBuilder, r 
 			return sql.RowsToRowIter(sql.NewRow(types.NewOkResult(0))), nil
 		}
 	}
-	createIter, err := b.Build(ctx, c.createNode, r)
-	if createIter != nil {
-		_ = createIter.Close(ctx)
-	}
+	savepoint, hasSavepoint, err := createCreateTableAsSavepoint(ctx)
 	if err != nil {
 		return nil, err
+	}
+	abort := func(cause error) (sql.RowIter, error) {
+		if hasSavepoint {
+			return nil, rollbackCreateTableAsSavepoint(ctx, savepoint, cause)
+		}
+		return nil, cause
+	}
+	createIter, err := b.Build(ctx, c.createNode, r)
+	if createIter != nil {
+		if closeErr := createIter.Close(ctx); closeErr != nil && err == nil {
+			err = closeErr
+		}
+	}
+	if err != nil {
+		return abort(err)
 	}
 	table, ok, err := db.GetTableInsensitive(ctx, c.createTable.Name())
 	if err != nil {
-		return nil, err
+		return abort(err)
 	}
 	if !ok {
-		return nil, sql.ErrTableCreatedNotFound.New(c.createTable.Name())
+		return abort(sql.ErrTableCreatedNotFound.New(c.createTable.Name()))
 	}
 	insert := plan.NewInsertInto(db, plan.NewResolvedTable(table, db, nil), c.tableCopier.Source, false, nil, nil, false)
-	return b.Build(ctx, insert, r)
+	insertIter, err := b.Build(ctx, insert, r)
+	if err != nil {
+		return abort(err)
+	}
+	if !hasSavepoint {
+		return insertIter, nil
+	}
+	if insertIter == nil {
+		insertIter = sql.RowsToRowIter()
+	}
+	return &createTableAsSavepointIter{
+		childIter: insertIter,
+		savepoint: savepoint,
+	}, nil
 }
 
 func (c *CreateTableAs) Schema(ctx *sql.Context) sql.Schema {
@@ -111,4 +140,122 @@ func (c *CreateTableAs) WithChildren(_ *sql.Context, children ...sql.Node) (sql.
 		return nil, fmt.Errorf("expected *plan.TableCopier child, found %T", children[0])
 	}
 	return NewCreateTableAs(tableCopier), nil
+}
+
+func createCreateTableAsSavepoint(ctx *sql.Context) (string, bool, error) {
+	tx := ctx.GetTransaction()
+	if tx == nil {
+		return "", false, nil
+	}
+	txSession, ok := ctx.Session.(sql.TransactionSession)
+	if !ok {
+		return "", false, nil
+	}
+	name := fmt.Sprintf("__doltgresql_ctas_%d", atomic.AddUint64(&createTableAsSavepointCounter, 1))
+	if err := txSession.CreateSavepoint(ctx, tx, name); err != nil {
+		return "", false, err
+	}
+	return name, true, nil
+}
+
+func rollbackCreateTableAsSavepoint(ctx *sql.Context, name string, cause error) error {
+	tx := ctx.GetTransaction()
+	if tx == nil {
+		return cause
+	}
+	txSession, ok := ctx.Session.(sql.TransactionSession)
+	if !ok {
+		return fmt.Errorf("%w; CREATE TABLE AS rollback failed: session does not implement sql.TransactionSession", cause)
+	}
+	if err := txSession.RollbackToSavepoint(ctx, tx, name); err != nil {
+		return fmt.Errorf("%w; CREATE TABLE AS rollback failed: %v", cause, err)
+	}
+	if err := txSession.ReleaseSavepoint(ctx, ctx.GetTransaction(), name); err != nil {
+		return fmt.Errorf("%w; CREATE TABLE AS savepoint release failed: %v", cause, err)
+	}
+	return cause
+}
+
+func releaseCreateTableAsSavepoint(ctx *sql.Context, name string) error {
+	tx := ctx.GetTransaction()
+	if tx == nil {
+		return nil
+	}
+	txSession, ok := ctx.Session.(sql.TransactionSession)
+	if !ok {
+		return nil
+	}
+	return txSession.ReleaseSavepoint(ctx, tx, name)
+}
+
+type createTableAsSavepointIter struct {
+	childIter sql.RowIter
+	savepoint string
+	closed    bool
+	done      bool
+	finalized bool
+}
+
+var _ sql.RowIter = (*createTableAsSavepointIter)(nil)
+
+func (i *createTableAsSavepointIter) Next(ctx *sql.Context) (sql.Row, error) {
+	if i.done {
+		return nil, io.EOF
+	}
+	i.done = true
+
+	rowsAffected := 0
+	for {
+		row, err := i.childIter.Next(ctx)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, i.abort(ctx, err)
+		}
+		if types.IsOkResult(row) {
+			rowsAffected += int(types.GetOkResult(row).RowsAffected)
+		} else {
+			rowsAffected++
+		}
+	}
+	if err := i.complete(ctx); err != nil {
+		return nil, err
+	}
+	return sql.NewRow(types.NewOkResult(rowsAffected)), nil
+}
+
+func (i *createTableAsSavepointIter) Close(ctx *sql.Context) error {
+	if i.finalized {
+		return nil
+	}
+	return i.complete(ctx)
+}
+
+func (i *createTableAsSavepointIter) complete(ctx *sql.Context) error {
+	if i.finalized {
+		return nil
+	}
+	if err := i.closeChild(ctx); err != nil {
+		return i.abort(ctx, err)
+	}
+	i.finalized = true
+	return releaseCreateTableAsSavepoint(ctx, i.savepoint)
+}
+
+func (i *createTableAsSavepointIter) abort(ctx *sql.Context, cause error) error {
+	if i.finalized {
+		return cause
+	}
+	_ = i.closeChild(ctx)
+	i.finalized = true
+	return rollbackCreateTableAsSavepoint(ctx, i.savepoint, cause)
+}
+
+func (i *createTableAsSavepointIter) closeChild(ctx *sql.Context) error {
+	if i.closed {
+		return nil
+	}
+	i.closed = true
+	return i.childIter.Close(ctx)
 }
